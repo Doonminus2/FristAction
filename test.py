f4ec9ac76b95cbbfdc199266176e4c1a8f937d08
pi_263486 <140317465+Doonminus2@users.noreply.github.com>
2025-03-14 13:47:39 +0700

    add function bye

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 def say_hello():
-print("Hello, GitHub Actions!")  # Indentation ผิดพลาด
+    print("Hello, GitHub Actions!")  # Indentation ผิดพลาด
+
+def sayBye():
+    print("bye guyyy")
 
 say_hello()
+sayBye()
